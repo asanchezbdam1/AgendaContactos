9addc08c87326d1260249f6d60af3dc4c399fac7
agaonaddam1 <agaonadam1@gmail.com>
2021-04-12 20:05:23 +0200

    Acabada clase Personal, no probada.

--- AgendaContactos/src/Personal.java
+++ AgendaContactos/src/Personal.java
@@ -1,8 +1,23 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-public class Personal extends Contacto{
+public class Personal extends Contacto {
+	private LocalDate fechaNacimiento;
+	private Relacion relacion;
 
-	public Personal() {
+	public Personal(String nombre, String apellido, String telefono, String email, String fechaNacimiento,
+			Relacion relacion) {
+		super(nombre, apellido, telefono, email);
+		this.fechaNacimiento = LocalDate.parse(fechaNacimiento, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+		this.relacion = relacion;
+	}
 
+	public void setFirmaEmail() {
+		super.setFirmaEmail("Un abrazo!!");
+	}
+
+	public boolean esCumpleaños() {
+		return fechaNacimiento.isEqual(LocalDate.now());
 	}
 
 }
