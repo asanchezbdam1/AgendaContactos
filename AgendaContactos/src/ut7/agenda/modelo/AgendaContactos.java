33f7da84cbfbaf34c896d44be7cda8f13e04d01c
asanchezbdam1 <asanchezbdam1@gmail.com>
2021-04-15 22:05:00 +0200

    Completados totalContactos, toString, personalesEnLetra de la clase AgendaContactos

--- AgendaContactos/src/ut7/agenda/modelo/AgendaContactos.java
+++ AgendaContactos/src/ut7/agenda/modelo/AgendaContactos.java
@@ -1,7 +1,10 @@
 package ut7.agenda.modelo;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 /**
  * 
  * @author Ander Gaona y Asier Sánchez
@@ -11,7 +14,7 @@ public class AgendaContactos {
 	private Map<Character, Set<Contacto>> agenda;
 
 	public AgendaContactos() {
-
+		agenda = new TreeMap<>();
 	}
 
 	public void añadirContacto() {
@@ -22,25 +25,37 @@ public class AgendaContactos {
 
 	}
 
-	public void totalContactos() {
-
+	public int totalContactos() {
+		int n = 0;
+		for (Character c : agenda.keySet()) {
+			n += agenda.get(c).size();
+		}
+		return n;
 	}
 
 	@Override
 	public String toString() {
-
-		return null;
+		StringBuilder sb = new StringBuilder();
+		for (Character c : agenda.keySet()) {
+			agenda.get(c).forEach(cont -> sb.append(cont.toString()));
+		}
+		return sb.toString();
 	}
 
 	public List<Contacto> buscarContactos(String texto) {
-
+		
 		return null;
 
 	}
 
 	public List<Personal> personalesEnLetra(char letra) {
-
-		return null;
+		List<Personal> personales = new ArrayList();
+		for (Contacto cont : agenda.get(letra)) {
+			if (cont instanceof Personal) {
+				personales.add((Personal) cont);
+			}
+		}
+		return personales;
 	}
 
 	public List<Personal> felicitar() {
@@ -49,7 +64,7 @@ public class AgendaContactos {
 	}
 
 	public void personalesPorRelacion() {
-
+		
 	}
 
 	public List<Personal> personalesOrdenadosPorFechaNacimiento(char letra) {
