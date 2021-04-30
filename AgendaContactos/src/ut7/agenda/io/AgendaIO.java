45ad89ced042488547d47438f7999749cbe45d47
agaonaddam1 <agaonadam1@gmail.com>
2021-04-30 20:04:39 +0200

    Modificado metodo importar

--- AgendaContactos/src/ut7/agenda/io/AgendaIO.java
+++ AgendaContactos/src/ut7/agenda/io/AgendaIO.java
@@ -1,7 +1,10 @@
 package ut7.agenda.io;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -29,11 +32,46 @@ public class AgendaIO {
 	 * 
 	 * @param La agenda a la que se quiere añadir los contactos
 	 */
-	public static void importar(AgendaContactos agenda) {
-		String[] contactos = obtenerLineasDatos();
-		for (String contacto : contactos) {
-			agenda.añadirContacto(parsearLinea(contacto));
+	public static int importar(AgendaContactos agenda, String nombre) {
+		File f = new File(nombre);
+		int errores = 0;
+		BufferedReader entrada = null;
+		try {
+			entrada = new BufferedReader(new FileReader(f));
+			String linea = entrada.readLine();
+			while (linea != null) {
+				try {
+					agenda.añadirContacto(parsearLinea(linea));
+					linea = entrada.readLine();
+				} catch (DateTimeParseException e) {
+					System.out.println("Error al parsear fecha " + e.getMessage());
+					errores++;
+				} catch (ArrayIndexOutOfBoundsException e) {
+					System.out.println("Error al leer la linea " + e.getMessage());
+					errores++;
+				} catch (EnumConstantNotPresentException e) {
+					System.out.println("Error al encontrar la relacion " + e.getMessage());
+					errores++;
+				} catch (IOException e) {
+					System.out.println("Error al cargar la linea " + e.getMessage());
+					errores++;
+				}
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("Archivo no encontrado " + e.getMessage());
+		} catch (IOException e1) {
+			System.out.println("Error en lectura " + e1.getMessage());
+		} finally {
+			try {
+				entrada.close();
+			} catch (NullPointerException e) {
+				System.out.println("Error al crear el lector " + e.getMessage());
+			} catch (IOException e) {
+				System.out.println("Error al cerrar el lector " + e.getMessage());
+			}
 		}
+
+		return errores;
 	}
 
 	/**
@@ -42,7 +80,8 @@ public class AgendaIO {
 	 * @param La linea a parsear
 	 * @return Un objeto Contacto
 	 */
-	private static Contacto parsearLinea(String linea) throws DateTimeParseException, ArrayIndexOutOfBoundsException, EnumConstantNotPresentException {
+	private static Contacto parsearLinea(String linea)
+			throws DateTimeParseException, ArrayIndexOutOfBoundsException, EnumConstantNotPresentException {
 		String[] datos = linea.split(",");
 		for (int i = 0; i < datos.length; i++) {
 			datos[i] = datos[i].trim();
@@ -58,7 +97,7 @@ public class AgendaIO {
 		}
 		return c;
 	}
-	
+
 	public static void exportarPersonales(AgendaContactos agenda, String fichero) throws IOException {
 		PrintWriter salida = new PrintWriter(new BufferedWriter(new FileWriter(new File(fichero))));
 		Map<Relacion, List<String>> personales = agenda.personalesPorRelacion();
